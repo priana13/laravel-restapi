5037cadad467d806c47ca6752a9b66a15567d39b
priana13 <herian.sap@gmail.com>
2021-03-09 20:37:32 +0700

    API Resource untuk memisahkan dengan response model dg selain API

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Post\PostResource;
 use Illuminate\Http\Request;
 use App\Post;
 use Illuminate\Support\Facades\Validator;
@@ -24,7 +25,8 @@ class PostController extends Controller
                 'messege' => 'Resource Not Found'], 404);
         }
 
-        return response()->json($data,200);
+        return new PostResource($data);
+        // return response()->json($data,200);
     }
 
     public function store(Request $request)
--- app/Post.php
+++ app/Post.php
@@ -8,6 +8,21 @@ class Post extends Model
 {
     protected $guarded = [];
 
+
+    //cara seperti ini bisa dibukan untuk menyembunyikan beberapa response
+    //akan tetapi tidak bisa di gunakan untuk API karena jika model ini
+    //akan digunakan selain di API maka akan jadi problem tersendiri
+    //untuk ini kita butuh untuk menggunakan php artisan sersource
+
+    protected $hidden = ['created_at','updated_at'];
+    protected $appends = ['tanggal'];
+    public function getTanggalAttribute()
+    {
+    	return $this->created_at->diffForHumans();
+    }
+
+
+
     public function user()
     {
         return $this->belongsTo(User::class);
